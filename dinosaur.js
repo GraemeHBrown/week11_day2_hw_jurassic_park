e4f9fc392aff03f0d7bf34752c8fb9d41f1085a8
Graeme Brown <graemehbrown@googlemail.com>
2018-02-13 17:23:52 +0000

    Added number of offspring property to Dinosaur and a count method to return this.

--- dinosaur.js
+++ dinosaur.js
@@ -1,5 +1,10 @@
 const Dinosaur = function(type){
   this.type = type;
+  this.numberOfOffspring = 0;
+}
+
+Dinosaur.prototype.countOffspring = function() {
+  return this.numberOfOffspring;
 }
 
 module.exports = Dinosaur;
--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const Dinosaur = require('../dinosaur.js');
 
-describe('Basket', function(){
+describe('Dinosaur', function(){
   let dinosaur;
 
   beforeEach(function(){
@@ -11,6 +11,12 @@ describe('Basket', function(){
   it('should have a type', function(){
     const actual = dinosaur.type;
     assert.strictEqual(actual, 'Velociraptor');
+  });
+
+  it('should have a number of offspring', function(){
+    dinosaur.numberOfOffspring = 1;
+    const actual = dinosaur.countOffspring();
+    assert.strictEqual(actual, 1);
   })
 
 });
